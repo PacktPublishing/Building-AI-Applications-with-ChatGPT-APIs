574618b08c41fef08dbbb916433fb213b2106fb7
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2024-04-29 13:45:32 -0400

    Update translate.py

--- Chapter10 Whisper/translate.py
+++ Chapter10 Whisper/translate.py
@@ -1,9 +1,9 @@
-import openai
+from openai import OpenAI
 import config
 
 # API Token
-openai.api_key = config.API_KEY
+client = OpenAI(api_key=config.API_KEY)
 
 whisper_file= open("files/german.mp3", "rb")
-result = openai.Audio.translate("whisper-1", whisper_file)
+result = client.audio.translations.create(model="whisper-1", file=whisper_file)
 print(result)
