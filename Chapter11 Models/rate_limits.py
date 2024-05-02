93f265541d3b45dead87c89a32472b048ce24c68
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2024-05-01 22:19:57 -0400

    Update rate_limits.py

--- Chapter11 Models/rate_limits.py
+++ Chapter11 Models/rate_limits.py
@@ -1,25 +1,11 @@
-import openai # for making OpenAI API requests
-
+import backoff 
+import openai
 import config
+from openai import OpenAI
+client = OpenAI(api_key=config.API_KEY)
 
-# Set up OpenAI API credentials
-openai.api_key = config.API_KEY
-
-num_stories = 10
-prompts = ["I was walking down the street and"] * num_stories
-
-# Perform batched completions with 10 stories per request
-response = openai.Completion.create(
-            model="curie",
-            prompt=prompts,
-            max_tokens=20,
-            )
-
-# Match completions to prompts by index
-stories = [""] * len(prompts)
-for choice in response.choices:
-    stories[choice.index] = prompts[choice.index] + choice.text
-
-# Print the generated stories
-for story in stories:
-    print(story)
+@backoff.on_exception(backoff.expo, openai.RateLimitError)
+def completions_with_backoff(**kwargs):
+    return client.completions.create(**kwargs)
+ 
+completions_with_backoff(model="gpt-3.5-turbo-instruct", prompt="I was walking down the street,")
