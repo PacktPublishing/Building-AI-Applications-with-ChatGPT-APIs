b3bd6fda2ca04e3b9f134c5befc16d653d513feb
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2024-05-01 22:19:01 -0400

    Update models.py

--- Chapter11 Models/models.py
+++ Chapter11 Models/models.py
@@ -1,50 +1,39 @@
-import openai
+from openai import OpenAI
 import config
 
-# Define the prompt and test questions
-prompt = "Estimate the square root of 121 and type a 'orange' after every digit of the square root"
+client = OpenAI(api_key=config.API_KEY)
 
-# Set up OpenAI API credentials
-openai.api_key = config.API_KEY
+# Define the prompt and test questions
+prompt = "Estimate the square root of 121 and type 'orange' after every digit of the square root result"
 
 # Define the model names and their corresponding IDs
 model_ids = {
-    "DAVINCI 003": {"model": "text-davinci-003", "cost": 0.02},
-    "DAVINCI 002": {"model": "text-davinci-002", "cost": 0.02},
-    "DAVINCI": {"model": "davinci", "cost": 0.02},
-    "GPT3.5 TURBO": {"model": "gpt-3.5-turbo", "cost": 0.002},
-    "GPT3.5 TURBO 0301": {"model": "gpt-3.5-turbo-0301", "cost": 0.002},
-    "GPT4": {"model": "gpt-4", "cost": 0.0045},
-    "CURIE": {"model": "text-curie-001", "cost": 0.002},
-    "BABBAGE": {"model": "text-babbage-001", "cost": 0.005},
+    "GPT3.5 TURBO": {"model": "gpt-3.5-turbo", "cost": 1.00},
+    "GPT3.5 TURBO 0125": {"model": "gpt-3.5-turbo-0125", "cost": 1.00},
+    "GPT4": {"model": "gpt-4", "cost": 45.00},
+    "GPT4 TURBO": {"model": "gpt-4-turbo", "cost": 20.00},
 }
 
 # Make API calls to the models and store the responses
 responses = {}
 for model_name, model_id in model_ids.items():
-    if "GPT" not in model_name:
-        response = openai.Completion.create(
-            engine=model_id["model"],
-            prompt=prompt,
-            max_tokens=50,
-            n=1,
-            stop=None,
-            temperature=0.7
-        )
-        responses[model_name] = [response.choices[0].text.strip(),
-                                 response['usage']['total_tokens']/1000*model_id["cost"]]
+    response = client.chat.completions.create(
+        model=model_id["model"],
+        messages=[
+            {"role": "user", "content": f"{prompt}"}
+        ]
+    )
+    responses[model_name] = [response.choices[0].message.content,
+                             response.usage.total_tokens * (model_id["cost"]/1000000)]
 
-    else:
-        response = openai.ChatCompletion.create(
-            model=model_id["model"],
-            messages=[
-                {"role": "user", "content": "I will ask you a question"},
-                {"role": "assistant", "content": "Ok"},
-                {"role": "user", "content": f"{prompt}"}
-            ]
-        )
-        responses[model_name] = [response["choices"][0]["message"]["content"],
-                                 response['usage']['total_tokens']/1000*model_id["cost"]]
+for model, response in responses.items():
+    print("\n----------------------------------------")
+    print(f"{model}: {response[0]}")
+    print(f"{model} COST: {response[1]}")
+
+print("----------------------------------------")
+
+
 
 
 for model, response in responses.items():
