f15d37a2151c0446466458a07fc466c1146065a4
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2024-06-14 17:47:56 -0400

    Update app.py

--- Chapter02 ChatGPTClone/app.py
+++ Chapter02 ChatGPTClone/app.py
@@ -7,11 +7,11 @@ __modified__= 03/03/2024
 """
 
 from flask import Flask, request, render_template
-import openai
+from openai import OpenAI
 import config
 
-openai.api_key = config.API_KEY
 
+client = OpenAI(api_key=config.API_KEY)
 app = Flask(__name__)
 
 conversation_history = []
@@ -25,18 +25,17 @@ def index():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
+
     model_engine = "gpt-3.5-turbo"
 
-    # Append user message to conversation history
     conversation_history.append({"role": "user", "content": userText})
-
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         model=model_engine,
-        messages=conversation_history  # Pass the entire conversation history to OpenAI
+        messages=conversation_history
     )
-
     # Append AI response to conversation history
-    ai_response = response["choices"][0]["message"]["content"]
+    ai_response = response.choices[0].message.content
+
     conversation_history.append({"role": "assistant", "content": ai_response})
 
     return ai_response
